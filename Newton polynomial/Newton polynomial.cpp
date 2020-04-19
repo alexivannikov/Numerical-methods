aaf328e312b369ecb26000536a44fd6994158946
Алексей Иванников <aleksejivannikoff@gmail.com>
2020-04-19 18:48:37 +0300

    first commit

---
+++ Newton polynomial/Newton polynomial.cpp
@@ -0,0 +1,253 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double** y = new double*[100]; //Выделение памяти для таблицы интерполяции
+	double x;
+	int n;
+
+	for (int i = 0; i < 100; i++) {
+		y[i] = new double[100];
+	}
+
+	cout << "Function:" << endl;
+	cout << "x\t" << "y\t" << endl;
+
+	for (int i = 0; i < 11; i++) { //Заполнение таблицы точками, сгенерированными проихводящей функцией
+		y[0][i] = i; 
+		cout << y[0][i]; 
+
+		y[1][i] = y[0][i] * y[0][i]; //Производящая функция y = x*x
+		cout << '\t' << y[1][i];
+
+		cout << '\n';
+	}
+
+	int r = 0;
+
+	while (r < 3) { //Можно посчитать три произвольно взятые точки
+		cout << "\n\nInput X:" << endl;
+		cin >> x;
+		
+		if (x < y[0][0] || x > y[0][10]) { 
+			/*Условие: если координата x интерполируемой точки
+			меньше координаты x первой точки таблицы или больше координаты x
+			последней точки таблицы (выход за границы массива), то ошибка*/
+			cout << "\nImpossible to calculate function value! Going abroad table.";
+		}
+		
+		/*Иначе: проводим интерполирование точки*/
+		else {
+			cout << "\nInput polynomial degree:" << endl; //Вводим степень полинома
+			cin >> n;
+			cout << endl;
+
+			int k = 0; //Счетчик в выражении (x0 - xn) в формуле для конечной разности (знаменатель формулы)
+			int t = -1; //Счетчик для обозначения x[i] в интервале интерполяции
+			int q = -1; //Счетчик для обозначения y[i] в интервале интерполяции
+
+			if ((int)x - n/2 < 0)  {
+				cout << "Intepolation interval:" << endl; //Вывод интервала интерполяции
+				cout << "\txi\t" << "\tyi\t" << endl;
+				
+				for (int i = (int)x; i <= (int)x + n; i++) {
+
+					t = t + 1;
+					y[0][i] = t;
+					cout << "x" << "[" << y[0][i] << "]";
+
+					y[1][i] = i; //Вывод координат x точек, попавших в интервал интерполяции
+					cout << '\t' << y[1][i];
+
+					q = q + 1;
+
+					y[2][i] = q;
+					cout << '\t' << "y" << "[" << y[2][i] << "]";
+
+					y[3][i] = y[1][i] * y[1][i];
+					cout << '\t' << y[3][i];
+
+					cout << '\n';
+				}
+				for (int j = 4, k = 1; j <= 4 + n, k < n + 1; j++, k++) {
+					for (int i = (int)x; i <= (int)x  + n; i++) {
+						y[j][i] = (y[j - 1][i] - y[j - 1][i + 1]) / (y[1][i] - y[1][i + k]);
+					}
+				}
+
+				cout << "\nFunction value: " << endl;
+
+				double a = 1;
+				double b = 0;
+				double c = 0;
+				int j = 0;
+				int i = 0;
+
+				for (i = (int)x, j = 4; i <= (int)x + n, j < 4 + n; i++, j++) {
+					a = a * (x - y[1][i]);
+					b = a * y[j][(int)x];
+					c += b;
+
+				}
+
+				cout << "P(x) = " << y[3][(int)x] + c;
+			}
+			
+			else if ((int)x + n/2 > 10) {
+				cout << "Intepolation interval:" << endl;
+				cout << "\txi\t" << "\tyi\t" << endl;
+
+				int t = -1;
+				int q = -1;
+
+				for (int i = (int)x - n; i <= (int)x; i++) {
+					t = t + 1;
+					y[0][i] = t;
+					cout << "x" << "[" << y[0][i] << "]";
+
+					y[1][i] = i;
+					cout << '\t' << y[1][i];
+
+					q = q + 1;
+
+					y[2][i] = q;
+					cout << '\t' << "y" << "[" << y[2][i] << "]";
+
+					y[3][i] = y[1][i] * y[1][i];
+					cout << '\t' << y[3][i];
+
+					cout << '\n';
+				}
+
+				for (int j = 4, k = 1; j <= 4 + n, k < n + 1; j++, k++) {
+					for (int i = (int)x - n; i <= (int)x; i++) {
+						y[j][i] = (y[j - 1][i] - y[j - 1][i + 1]) / (y[1][i] - y[1][i + k]);
+					}
+				}
+
+				cout << "\nFunction value: " << endl;
+
+				double a = 1;
+				double b = 0;
+				double c = 0;
+				int j = 0;
+				int i = 0;
+
+				for (i = (int)x-n, j = 4; i <= (int)x, j < 4 + n; i++, j++) {
+					a = a * (x - y[1][i]);
+					b = a * y[j][(int)x-n];
+					c += b;
+
+				}
+
+				cout << "P(x) = " << y[3][(int)x - n] + c;
+			}
+
+			else {
+				cout << "Intepolation interval:" << endl;
+				cout << "\txi\t" << "\tyi\t" << endl;
+				
+				int t = -1;
+				int q = -1;
+
+				for (int i = (int)x - n / 2; i <= ((int)x - n / 2) + n; i++) {
+					t = t + 1;
+					y[0][i] = t;
+					cout << "x"<<"["<< y[0][i] <<"]";
+
+					y[1][i] = i;
+					cout <<'\t' <<y[1][i];
+
+					q = q + 1;
+					
+					y[2][i] = q;
+					cout <<'\t'<< "y" << "[" << y[2][i] << "]";
+					
+					y[3][i] = y[1][i] * y[1][i];
+					cout << '\t' << y[3][i];
+
+					cout << '\n';
+				}
+				for (int j = 4, k = 1; j <= 4 + n, k < n + 1; j++, k++) {
+					for (int i = (int)x - n / 2; i <= ((int)x - n / 2) + n; i++) {
+						y[j][i] = (y[j - 1][i] - y[j - 1][i + 1]) / (y[1][i] - y[1][i + k]);
+					}
+				}
+
+				cout << "\nFunction value: " << endl;
+
+				double a = 1;
+				double b = 0;
+				double c = 0;
+				int j = 0;
+				int i = 0;
+
+				for (i = (int)x - n / 2, j = 4; i <= ((int)x - n / 2) + n, j < 4 + n; i++, j++) {
+					a = a * (x - y[1][i]);
+					b = a * y[j][(int)x - n / 2];
+					c += b;
+
+				}
+
+				cout << "P(x) = " << y[3][(int)x - n / 2] + c;
+			}
+		}
+		r++;
+	}
+	
+	double** z = new double* [100];
+	double p = 0;
+	int m;
+	
+	for (int u = 0; u < 100; u++) {
+		z[u] = new double[100];
+	}
+
+	r = 0;
+
+	double g = -0.5;
+
+	while (r < 3) {
+		cout << "\n\nInput polynomial degree:" << endl;
+		cin >> m;
+
+		for (int u = -1; u < 20; u++) {
+			g = g + 0.5;
+			z[1][u] = g;
+			z[0][u] = cos(z[1][u]) - z[1][u];
+		}
+
+		int l = 0;
+
+		for (int w = 2, l = 1; w <= 2 + m, l < m + 1; w++, l++) {
+			for (int u = (int)p; u <= (int)p + m; u++) {
+				z[w][u] = (z[w - 1][u] - z[w - 1][u + 1]) / (z[0][u] - z[0][u + l]);
+			}
+		}
+
+		cout << "\nFunction y = cos(x) - x root: \n" << endl;
+
+		double d = 1;
+		double e = 0;
+		double f = 0;
+		int w = 0;
+		int u = 0;
+
+		for (u = (int)p, w = 2; u <= (int)p + m, w < 2 + m; u++, w++) {
+			d = d * (p - z[0][u]);
+			e = d * z[w][(int)p];
+			f += e;
+
+		}
+
+		cout << "x = " << z[1][(int)p] + f;
+
+			r++;
+	}
+}
+	
+
---
+++ Standart deviation/Среднеквадратичное отклонение/MathLab2/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MathLab2
+{
+    public partial class Form1 : Form
+    {
+        static public List<Point> points = new List<Point>();
+        static public List<Point> result = new List<Point>();
+        static public List<double> coefficients = new List<double>();
+        static public int POINTS = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DataInit();
+            TableInit();
+            GraphInit();
+            Calculation();
+        }
+
+        public void GraphInit() { 
+
+            chart1.Series.Clear();
+
+            chart1.ChartAreas[0].AxisX.Interval = 0.5;
+            chart1.ChartAreas[0].AxisX.Minimum = -POINTS/2;
+            chart1.ChartAreas[0].AxisX.Maximum = POINTS / 2;
+
+            chart1.ChartAreas[0].AxisY.Interval = 0.5;
+            chart1.ChartAreas[0].AxisY.Minimum = -POINTS / 2;
+            chart1.ChartAreas[0].AxisY.Maximum = POINTS / 2;
+
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Blue,
+                IsVisibleInLegend = false,
+                BorderWidth=5,
+                ChartType = SeriesChartType.Point
+            };
+
+            chart1.Series.Add(series1);
+            foreach(Point data in points)
+                chart1.Series[0].Points.AddXY(data.x, data.y);
+
+            var series2 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series2",
+                Color = System.Drawing.Color.Red,
+                IsVisibleInLegend = false,
+                BorderWidth=5,
+                ChartType = SeriesChartType.Spline
+            };
+
+            chart1.Series.Add(series2);
+            foreach(Point point in result)
+                chart1.Series[1].Points.AddXY(point.x, point.y);
+
+            chart1.Invalidate();
+
+        }
+
+        public void DataInit(){
+            Random rnd = new Random();
+
+            for (int i = -POINTS / 2; i <= POINTS; i++) {
+                Point point = new Point(i, rnd.NextDouble()*rnd.Next(-POINTS/ 2, POINTS/2));
+                points.Add(point);
+            }
+        }
+
+        public void TableInit() {
+            dataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dataGridView1.Columns[0].DefaultCellStyle.Format = "n2";
+            dataGridView1.Columns[1].DefaultCellStyle.Format = "n3";
+            dataGridView1.Columns[2].DefaultCellStyle.Format = "n3";
+
+            for (int i = 0; i <= POINTS; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1[0, i].Value = Convert.ToString(Math.Round((points[i].x), 2));
+                dataGridView1[1, i].Value = Convert.ToString(Math.Round((points[i].y), 3));
+                dataGridView1[2, i].Value = Convert.ToString(Math.Round((points[i].weight), 3));
+            }
+
+        }
+
+        public void Calculation() {
+            for (int i = 0; i <= POINTS; i++) {
+                points[i].x = Convert.ToDouble(dataGridView1[0, i].Value);
+                points[i].y = Convert.ToDouble(dataGridView1[1, i].Value);
+                points[i].weight = Convert.ToDouble(dataGridView1[2, i].Value);
+            }
+
+            MatrixCreation();
+            EquationBuild();
+            GraphInit();
+        }
+
+        public void MatrixCreation() {
+            //Степень полинома
+            int n;
+
+            if (domainUpDown1.SelectedIndex == -1)
+                n = 0;
+            else
+                n = Convert.ToInt32(domainUpDown1.SelectedItem.ToString());
+          
+            int str = n + 1; //Количество строк в матрице 
+            int stlb = n + 2; //Количество столбцов в матрице 
+
+            double sumItems = 0;
+
+            int xItems = str + (str - 1);
+            int yItems = str;
+
+            double[,] matrix = new double[str, stlb]; //Матрица 
+            double[] XArray = new double[xItems]; //Вектор сумм ro*x
+            double[] YArray = new double[yItems]; //Вектор сумм ro*x*y
+            
+            for (int i = 0; i < xItems; i++) //От 0 до 2*n
+            {
+                for (int j = 0; j <= POINTS; j++)
+                    sumItems += Math.Pow(points[j].x, i) * points[j].weight;
+                XArray[i] = sumItems;
+                sumItems = 0;
+            }
+            
+            sumItems = 0;
+
+            for (int i = 0; i < yItems; i++) {
+                for (int j = 0; j <= POINTS; j++)
+                    sumItems += points[j].y * Math.Pow(points[j].x, i)*points[j].weight;
+                YArray[i] = sumItems;
+                sumItems = 0;
+            }
+
+            //Заполнение матрицы построчно
+            int item = 0, count = 0, index = 0;
+
+             for (int i = 0; i < str; i++) {
+                 for (; item < str; item++)
+                     matrix[i, item] = XArray[index++];
+
+                    matrix[i, item] = YArray[i];
+                 item = 0; count++; index = count;               
+            }
+
+            string s;
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Matrix");
+
+            for (int i = 0; i < str; i++)
+            {
+                s = "";
+                for (int j = 0; j < str+1; j++)
+                    s += (matrix[i, j]).ToString() + " " + " ";
+                listBox1.Items.Add(s);
+            }
+
+            listBox1.Items.Add(" ");
+            listBox1.Items.Add("Diagonal Matrix");
+
+            for (int i = 0; i < str; i++)
+            {
+                s = "";
+                for (int j = 0; j < str+1; j++)
+                {
+                    GaussianMethod(matrix, n);
+                    s += (matrix[i, j]).ToString() + " " + " ";
+                }
+                listBox1.Items.Add(s);
+            }
+
+        }
+
+            public void GaussianMethod(double[,] matrix, int n) {
+            int eqQty = n + 1;
+            double mult;
+            double[] tempCoff = new double[eqQty];
+
+            coefficients = new List<double>();
+
+            for (int i = 0; i < (eqQty - 1); i++)
+            {
+                for (int j = i + 1; j < eqQty; j++)
+                {
+                    mult = (-1) * matrix[j, i] / matrix[i, i];
+                    for (int k = i; k <= eqQty; k++)
+                        matrix[j, k] += matrix[i, k] * mult;
+                }
+            }
+         
+            double number;
+
+            for (int i = (eqQty - 1); i >= 0; i--) {
+                number = matrix[i, eqQty];
+                for (int j = (eqQty - 1); j > i; j--)
+                    number -= matrix[i, j]*tempCoff[j];
+
+                tempCoff[i] = number / matrix[i, i];
+            }
+
+            foreach (double num in tempCoff)
+                coefficients.Add(num);
+        }
+
+        public void EquationBuild() {
+            double res; int power;
+
+            result = new List<Point>();
+
+            for (int i = 0; i <= POINTS; i++) {
+                power = 0; res = 0;
+                foreach (double val in coefficients)
+                {
+                    res += val * Math.Pow(points[i].x, power);
+                    power++;
+                }
+                Point p = new Point(points[i].x, res, points[i].weight);
+                result.Add(p);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculation();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class Point{
+        public double x;
+        public double y;
+        public double weight;
+
+        public Point(){
+            x=0; y=0; weight=1;
+        }
+
+        public Point(double ix, double iy, double iw=1) {
+            x = ix; y = iy; weight = iw;
+        }
+    }
+}
